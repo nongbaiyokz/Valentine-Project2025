99add9f1e9c45c64aa6fad68410965523a0c34bc
NongBaiYokz <gle2012za@gmail.com>
2025-02-06 13:21:48 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 import Valentine from "./components/Valentine";
-import Loading from "./components/Loading";
-import Calculator from "./components/Calculator";
 
 export default function Home() {
   return (
