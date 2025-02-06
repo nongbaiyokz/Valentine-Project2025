45c83aa4a4f0de065443d2f01970db9448e41aa0
NongBaiYokz <gle2012za@gmail.com>
2025-02-06 12:53:11 +0700

    Update Valentine.tsx

--- app/components/Valentine.tsx
+++ app/components/Valentine.tsx
@@ -8,41 +8,7 @@ const Valentine = () => {
         
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-            <motion.div
-                    className="absolute top-0 left-0 w-full h-full pointer-events-none"
-                    initial={{ y: '100%' }}
-                    animate={{ y: '-100%' }}
-                    transition={{
-                        duration: 100,
-                        loop: Infinity,
-                        ease: 'linear'
-                    }}
-                >
-                    <div>
-                        {[...Array(10)].map((_, index) => (
-                            <motion.img
-                                key={index}
-                                src="https://media.discordapp.net/attachments/1283830529933840507/1336932987983499305/Heart-Balloon-PNG-Background.png?ex=67a59b5f&is=67a449df&hm=1ec550c195592b9389cdf470f5af30431c957dcfcb0a4e168756bdd10b3a3308&=&format=webp&quality=lossless&width=540&height=701"
-                                alt="Balloon"
-                                className="absolute"
-                                style={{
-                                    left: `${Math.random() * 100}vw`,
-                                    top: `${Math.random() * 100}vh`,
-                                    width: `${Math.random() * 2 + 6}vw`,
-                                    opacity: 0.6,
-                                }}
-                                initial={{ y: '100%' }}
-                                animate={{ y: '-100%' }}
-                                transition={{
-                                    duration: Math.random() * 50 + 50,
-                                    loop: Infinity,
-                                    delay: Math.random() * 10,
-                                    ease: 'backIn'
-                                }}
-                            />
-                        ))}
-                    </div>
-                </motion.div>
+           
                 <motion.div
                     initial={{ opacity: 0, x: -100 }}
                     animate={{ opacity: 1, x: 0 }}
